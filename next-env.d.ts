181f62ac703f6239a232cfc2dda79601199892e1
TatsuyaYamamoto <t28.is.tatsuya@gmail.com>
2021-03-23 23:11:42 +0900

    build: use typescript

---
+++ next-env.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="next" />
+/// <reference types="next/types/global" />
+
+declare module '*.css' {
+  interface IClassNames {
+    [className: string]: string
+  }
+
+  const classNames: IClassNames;
+  export = classNames;
+}

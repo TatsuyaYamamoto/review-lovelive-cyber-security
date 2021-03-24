4e22e639f9f6e027f32b48ce4b07af4522661e80
TatsuyaYamamoto <t28.is.tatsuya@gmail.com>
2021-03-24 22:28:09 +0900

    show timeline demo

--- pages/chapter-2.tsx
+++ pages/chapter-2.tsx
@@ -1,11 +1,26 @@
-import {} from "react";
+import { useState } from "react";
 import { NextPage } from "next";
+
 import AppLayout from "../src/components/AppLayout";
+import TwitterTimelineDemo from "../src/components/TwitterTimelineDemo/TwitterTimelineDemo";
+import Textbox from "../src/components/Textbox";
+import styles from "../src/styles/chapter-1.module.scss";
+
+import script from "../src/script/chapter1";
 
 const Chapter2: NextPage = () => {
+  const [scriptIndex, setScriptIndex] = useState(0);
+
+  const onClick = () => {
+    setScriptIndex((value) => value + 1);
+  };
+
+  const currentScript = script[scriptIndex];
+
   return (
-    <AppLayout>
-      <div>Chapter2</div>
+    <AppLayout onClick={onClick}>
+      <TwitterTimelineDemo />
+      <Textbox className={styles.textbox} script={currentScript} />
     </AppLayout>
   );
 };
---
+++ src/components/TwitterTimelineDemo/TweetItem.tsx
@@ -0,0 +1,53 @@
+import { FC } from "react";
+
+import IconButton from "@material-ui/core/IconButton";
+import RtIcon from "@material-ui/icons/Repeat";
+import FavIcon from "@material-ui/icons/Favorite";
+
+import styles from "./TweetItem.module.scss";
+
+export interface TweetDatum {
+  imgUrl: string;
+  displayName: string;
+  screenName: string;
+  text: string;
+  rt: number;
+  fav: number;
+}
+
+export interface TweetItemProps {
+  datum: TweetDatum;
+}
+
+const TweetItem: FC<TweetItemProps> = (props) => {
+  const { imgUrl, displayName, screenName, text, rt, fav } = props.datum;
+
+  return (
+    <li className={styles.tweet}>
+      <img src={imgUrl} className={styles.icon} />
+      <div>
+        <div className={styles.names}>
+          <span className={styles.displayName}>{displayName}</span>
+          <span className={styles.screenName}>{screenName}</span>
+        </div>
+        <div className={styles.content}>{text}</div>
+        <div className={styles.footer}>
+          <div>
+            <IconButton>
+              <RtIcon />
+            </IconButton>
+            <span>{rt}</span>
+          </div>
+          <div>
+            <IconButton>
+              <FavIcon />
+            </IconButton>
+            <span>{fav}</span>
+          </div>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default TweetItem;

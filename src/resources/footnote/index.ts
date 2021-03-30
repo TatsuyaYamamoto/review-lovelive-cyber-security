625064aace24cb49822f37c2de22e15c8edbc7f4
TatsuyaYamamoto <t28.is.tatsuya@gmail.com>
2021-03-30 16:17:42 +0900

    add footnote

--- src/resources/footnote/index.ts
+++ src/resources/footnote/index.ts
@@ -1,7 +1,17 @@
-import cyber_security_hashtag from "./cyber-security-hashtag.mdx";
+import cyber_security_awareness_month from "./cyber_security_awareness_month.mdx";
+import cyber_security_hashtag from "./cyber_security_hashtag.mdx";
+import dia_love_password from "./dia_love_password.mdx";
+import end_3_18 from "./end_3_18.mdx";
+import motto from "./motto.mdx";
+import two_factor_auth from "./two_factor_auth.mdx";
 
 const footnotes = {
+  cyber_security_awareness_month,
   cyber_security_hashtag,
+  dia_love_password,
+  end_3_18,
+  motto,
+  two_factor_auth,
 } as const;
 
 export type FootnoteId = keyof typeof footnotes;

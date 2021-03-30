edd11705a173e4864bf8603027da4a8e08177f2d
TatsuyaYamamoto <t28.is.tatsuya@gmail.com>
2021-03-31 01:59:31 +0900

    set backgdoun color of timeline

--- src/components/TwitterTimelineDemo/TwitterTimelineDemo.tsx
+++ src/components/TwitterTimelineDemo/TwitterTimelineDemo.tsx
@@ -1,111 +1,13 @@
-import TweetItem from "./TweetItem";
+import { timelineData } from "@/resources/timeline";
 
+import TweetItem from "./TweetItem";
 import styles from "./TwitterTimelineDemo.module.scss";
 
 const TwitterTimelineDemo = () => {
-  const data = [
-    {
-      imgUrl: "https://pbs.twimg.com/profile_images/3144229927/1ebde74324e5fb5d91ec6aa9bc3024fa_reasonably_small.gif",
-      displayName: "name1",
-      screenName: "name1",
-      text: "hogehogehoge",
-      rt: 0,
-      fav: 0,
-    },
-    {
-      imgUrl: "https://pbs.twimg.com/profile_images/3144229927/1ebde74324e5fb5d91ec6aa9bc3024fa_reasonably_small.gif",
-      displayName: "name1",
-      screenName: "name1",
-      text: "hogehogehoge",
-      rt: 0,
-      fav: 0,
-    },
-    {
-      imgUrl: "https://pbs.twimg.com/profile_images/3144229927/1ebde74324e5fb5d91ec6aa9bc3024fa_reasonably_small.gif",
-      displayName: "name1",
-      screenName: "name1",
-      text: "hogehogehoge",
-      rt: 0,
-      fav: 0,
-    },
-    {
-      imgUrl: "https://pbs.twimg.com/profile_images/3144229927/1ebde74324e5fb5d91ec6aa9bc3024fa_reasonably_small.gif",
-      displayName: "name1",
-      screenName: "name1",
-      text: "hogehogehoge",
-      rt: 0,
-      fav: 0,
-    },
-    {
-      imgUrl: "https://pbs.twimg.com/profile_images/3144229927/1ebde74324e5fb5d91ec6aa9bc3024fa_reasonably_small.gif",
-      displayName: "name1",
-      screenName: "name1",
-      text: "hogehogehoge",
-      rt: 0,
-      fav: 0,
-    },
-    {
-      imgUrl: "https://pbs.twimg.com/profile_images/3144229927/1ebde74324e5fb5d91ec6aa9bc3024fa_reasonably_small.gif",
-      displayName: "name1",
-      screenName: "name1",
-      text: "hogehogehoge",
-      rt: 0,
-      fav: 0,
-    },
-    {
-      imgUrl: "https://pbs.twimg.com/profile_images/3144229927/1ebde74324e5fb5d91ec6aa9bc3024fa_reasonably_small.gif",
-      displayName: "name1",
-      screenName: "name1",
-      text: "hogehogehoge",
-      rt: 0,
-      fav: 0,
-    },
-    {
-      imgUrl: "https://pbs.twimg.com/profile_images/3144229927/1ebde74324e5fb5d91ec6aa9bc3024fa_reasonably_small.gif",
-      displayName: "name1",
-      screenName: "name1",
-      text: "hogehogehoge",
-      rt: 0,
-      fav: 0,
-    },
-    {
-      imgUrl: "https://pbs.twimg.com/profile_images/3144229927/1ebde74324e5fb5d91ec6aa9bc3024fa_reasonably_small.gif",
-      displayName: "name1",
-      screenName: "name1",
-      text: "hogehogehoge",
-      rt: 0,
-      fav: 0,
-    },
-    {
-      imgUrl: "https://pbs.twimg.com/profile_images/3144229927/1ebde74324e5fb5d91ec6aa9bc3024fa_reasonably_small.gif",
-      displayName: "name1",
-      screenName: "name1",
-      text: "hogehogehoge",
-      rt: 0,
-      fav: 0,
-    },
-    {
-      imgUrl: "https://pbs.twimg.com/profile_images/3144229927/1ebde74324e5fb5d91ec6aa9bc3024fa_reasonably_small.gif",
-      displayName: "name1",
-      screenName: "name1",
-      text: "hogehogehoge",
-      rt: 0,
-      fav: 0,
-    },
-    {
-      imgUrl: "https://pbs.twimg.com/profile_images/3144229927/1ebde74324e5fb5d91ec6aa9bc3024fa_reasonably_small.gif",
-      displayName: "name1",
-      screenName: "name1",
-      text: "hogehogehoge",
-      rt: 0,
-      fav: 0,
-    },
-  ];
-
   return (
     <div className={styles.timelineWrapper}>
       <ul className={styles.timeline}>
-        {data.map((datum, index) => (
+        {timelineData.map((datum, index) => (
           <TweetItem key={index} datum={datum} />
         ))}
         <li className={styles.lastTweet}>
---
+++ src/resources/timeline.ts
@@ -0,0 +1,110 @@
+export const timelineData = [
+  {
+    imgUrl:
+      "https://pbs.twimg.com/profile_images/3144229927/1ebde74324e5fb5d91ec6aa9bc3024fa_reasonably_small.gif",
+    displayName: "name1",
+    screenName: "name1",
+    text: "hogehogehoge",
+    rt: 0,
+    fav: 0,
+  },
+  {
+    imgUrl:
+      "https://pbs.twimg.com/profile_images/3144229927/1ebde74324e5fb5d91ec6aa9bc3024fa_reasonably_small.gif",
+    displayName: "name1",
+    screenName: "name1",
+    text: "hogehogehoge",
+    rt: 0,
+    fav: 0,
+  },
+  {
+    imgUrl:
+      "https://pbs.twimg.com/profile_images/3144229927/1ebde74324e5fb5d91ec6aa9bc3024fa_reasonably_small.gif",
+    displayName: "name1",
+    screenName: "name1",
+    text: "hogehogehoge",
+    rt: 0,
+    fav: 0,
+  },
+  {
+    imgUrl:
+      "https://pbs.twimg.com/profile_images/3144229927/1ebde74324e5fb5d91ec6aa9bc3024fa_reasonably_small.gif",
+    displayName: "name1",
+    screenName: "name1",
+    text: "hogehogehoge",
+    rt: 0,
+    fav: 0,
+  },
+  {
+    imgUrl:
+      "https://pbs.twimg.com/profile_images/3144229927/1ebde74324e5fb5d91ec6aa9bc3024fa_reasonably_small.gif",
+    displayName: "name1",
+    screenName: "name1",
+    text: "hogehogehoge",
+    rt: 0,
+    fav: 0,
+  },
+  {
+    imgUrl:
+      "https://pbs.twimg.com/profile_images/3144229927/1ebde74324e5fb5d91ec6aa9bc3024fa_reasonably_small.gif",
+    displayName: "name1",
+    screenName: "name1",
+    text: "hogehogehoge",
+    rt: 0,
+    fav: 0,
+  },
+  {
+    imgUrl:
+      "https://pbs.twimg.com/profile_images/3144229927/1ebde74324e5fb5d91ec6aa9bc3024fa_reasonably_small.gif",
+    displayName: "name1",
+    screenName: "name1",
+    text: "hogehogehoge",
+    rt: 0,
+    fav: 0,
+  },
+  {
+    imgUrl:
+      "https://pbs.twimg.com/profile_images/3144229927/1ebde74324e5fb5d91ec6aa9bc3024fa_reasonably_small.gif",
+    displayName: "name1",
+    screenName: "name1",
+    text: "hogehogehoge",
+    rt: 0,
+    fav: 0,
+  },
+  {
+    imgUrl:
+      "https://pbs.twimg.com/profile_images/3144229927/1ebde74324e5fb5d91ec6aa9bc3024fa_reasonably_small.gif",
+    displayName: "name1",
+    screenName: "name1",
+    text: "hogehogehoge",
+    rt: 0,
+    fav: 0,
+  },
+  {
+    imgUrl:
+      "https://pbs.twimg.com/profile_images/3144229927/1ebde74324e5fb5d91ec6aa9bc3024fa_reasonably_small.gif",
+    displayName: "name1",
+    screenName: "name1",
+    text: "hogehogehoge",
+    rt: 0,
+    fav: 0,
+  },
+  {
+    imgUrl:
+      "https://pbs.twimg.com/profile_images/3144229927/1ebde74324e5fb5d91ec6aa9bc3024fa_reasonably_small.gif",
+    displayName: "name1",
+    screenName: "name1",
+    text: "hogehogehoge",
+    rt: 0,
+    fav: 0,
+  },
+  {
+    imgUrl:
+      "https://pbs.twimg.com/profile_images/3144229927/1ebde74324e5fb5d91ec6aa9bc3024fa_reasonably_small.gif",
+    displayName: "name1",
+    screenName: "name1",
+    text: "hogehogehoge",
+    rt: 0,
+    fav: 0,
+  },
+];

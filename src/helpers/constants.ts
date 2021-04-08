381a9615cb0c9a83382298c6674a82157a8bd3b8
TatsuyaYamamoto <t28.is.tatsuya@gmail.com>
2021-04-08 13:01:17 +0900

    fix: edit app url

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,2 +1,2 @@
-export const APP_URL = `http://review-lovelive-cyber-security.t28.dev`;
+export const APP_URL = `https://review-lovelive-cyber-security.t28.dev`;
 export const GA_TRACKING_ID = `G-WFJ7ZPL6J3`;
